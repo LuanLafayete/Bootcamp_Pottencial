e4d110f25b2282bdd50b5c26da908c538bfb4792
Luã Lafayete <luanlafayete@gmail.com>
2022-12-13 11:13:05 -0300

    Adicionando o Modulo API

---
+++ ModuloAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+/*
+Qual é o comando que faz para gerar um novo console
+    dotnet new console
+
+Qual é o comando que inicia uma WEB API no Visual Code:
+    dotnet new webapi
+
+Qual é o comando que para compilar sem pausar
+    dotnet watch run
+
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }  
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+            return Ok(new { mensagem });
+        }
+    }
+}
